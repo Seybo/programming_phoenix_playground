d95646752e7309a1f11363130f4edd5d0d71d235
seybo <michaelatworkspace@gmail.com>
2020-04-27 13:45:19 +0530

    add Users#show

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -3,7 +3,15 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
 
   def index(conn, _params) do
+    # TODO: mikhail
+    # require IEx
+    # IEx.pry()
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
   end
 end
